36f2cfc31861f30a4fe9fc17c198bf979bfb3aea
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-04-18 10:26:13 +0200

    yes

--- ICT1512_JS/js01/project02/project01-02_txt.js
+++ ICT1512_JS/js01/project02/project01-02_txt.js
@@ -2,10 +2,12 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+      Author: Zainap Van Blerck
+      Date:   2024/04/18
 
     Filename: project01-02.js
 */
 
 //define variables for service name and service speed
+
+let serviceName = "Basic", 

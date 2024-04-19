082b182a84a6f5b21de4dc980d0d1e688a3089f8
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-04-19 10:28:20 +0200

    yes

---
+++ ICT1512_JS/js02/chapter/js02.js
@@ -0,0 +1,25 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: Zainap Van Blerck
+      Date:   2024/04/19
+
+      Filename: js02.js
+ */
+
+//set thr form's default values
+
+function setupForm(){
+
+      document.getElementById("photoNum").value = 1;
+      document.getElementById("photoHrs").value = 2;
+      document.getElementById("makeBook").checked = false;
+      document.getElementById("photoRights").checked = false;
+      document.getElementById("photoDist").value = 0;
+
+
+}
+
--- soemthiing.js
+++ soemthiing.js
@@ -2,4 +2,3 @@ function writeMsg(date, status) {
     window.alert("Today is " + date + ". Your order is " + status);
    }
    
-   writeMsg("2024/04/19","completed");

f304c556875758b5087d4abbef327e5c009c112b
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-04-22 11:48:50 +0200

    yes

--- soemthiing.js
+++ soemthiing.js
@@ -2,3 +2,6 @@ function writeMsg(date, status) {
     window.alert("Today is " + date + ". Your order is " + status);
    }
    
+   let socialSecurityNumber = "123-45-6789";
+   let checkVar = isNaN(socialSecurityNumber);
+   document.write(checkVar);

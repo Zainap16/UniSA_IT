667b6f95a1f68625649479e75f7d2f195cd996ae
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-04-22 14:58:22 +0200

    ehehhe

--- ICT1512_JS/js02/chapter/js02.js
+++ ICT1512_JS/js02/chapter/js02.js
@@ -43,6 +43,19 @@ function getEstimate(){
       let hours = document.getElementById("photoHrs").value;
       let distance = document.getElementById("photoDist").value;
 
+      // Add the cost of photographers for the hours covered
+      totalCost += photographers * hours * EMP_COSST;
+
+      // Add the cost of distance per photographer per mile
+      totalCost += photographers * distance * TRAVEL_COST;
+
+      let buyBook = document.getElementById("makeBook").checked;
+
+      let buyRights = document.getElementById("photoRights").checked;
+      // Add the cost of the book if purchased
+      totalCost += buyBook ? BOOK_COST : 0;
+      // Add the cost of photo rights if purchased
+      totalCost += buyRights ? REPRO_COST : 0;
 }
 
 

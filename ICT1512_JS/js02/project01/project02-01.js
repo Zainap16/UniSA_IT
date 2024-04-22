98b2494693e26cf7af4d34b0064df1eaeb49da72
Zainap16 <zvanblerck1@gmail.com>
2024-04-22 22:17:24 +0200

    tm's problem

--- ICT1512_JS/js02/project01/project02-01.js
+++ ICT1512_JS/js02/project01/project02-01.js
@@ -3,9 +3,15 @@
       Project 02-01
 
       Celsius <-> Farenheit Coverter
-      Author: 
-      Date:   
+      Author: Zainap Van Blerck
+      Date:   2024/04/22 
 
       Filename: project02-01.js
  */
+
+// Celsius 5 (Fahrenheit 2 32)/1.8
+
+// Fahrenheit 5 Celsius 3 1.8 1 32
+
+
 

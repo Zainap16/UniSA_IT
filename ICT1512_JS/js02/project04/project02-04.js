ce6ea9c537152c9b795cceddbe491f93429844e8
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-04-23 13:31:11 +0200

    eh

--- ICT1512_JS/js02/project04/project02-04.js
+++ ICT1512_JS/js02/project04/project02-04.js
@@ -3,12 +3,12 @@
       Project 02-04
 
       Application to calculate the cost of a restaurant order plus tax
-      Author: 
-      Date:   
+      Author: Zainap Van Blerck
+      Date:   2024/04/23
 
       Filename: project02-04.js
  */
- 
+   const CHICKEN_PRICE = 10.95, HALIBUT_PRICE = 13.95, BURGER_PRICE = 9.95, SALMON_PRICE = 18.95, SALAD_PRICE = 7.95, SALES_TAX = 0.07;
 
 
 

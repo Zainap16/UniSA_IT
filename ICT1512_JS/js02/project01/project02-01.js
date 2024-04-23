dc63584e89cc2726a80f27f2d123bf74bae99074
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-04-23 10:32:06 +0200

    completed project 1

--- ICT1512_JS/js02/project01/project02-01.js
+++ ICT1512_JS/js02/project01/project02-01.js
@@ -13,5 +13,35 @@
 
 // Fahrenheit 5 Celsius 3 1.8 1 32
 
+function FahrenheitToCelsius(degree){
 
+      return (degree - 32) / 1.8;
+};
+
+function CelsiusToFahrenheit(degree){
+
+      return (degree * 1.8) + 32;
+};
+
+//stores the cValue
+const cValueElement = document.getElementById("cValue");
+
+cValueElement.onchange = function() {
+
+      let cDegree = cValueElement.value;
+
+      document.getElementById("fValue").value = CelsiusToFahrenheit(cDegree);
+
+};
+
+//attaches value
+const fValueElement = document.getElementById("fValue");
+//anon function using onchange
+fValueElement.onchange = function(){
+//store fDegree everytime it changes since onchange
+      let fDegree = fValueElement.value;
+
+      document.getElementById("cValue").value = FahrenheitToCelsius(fDegree);
+
+};
 

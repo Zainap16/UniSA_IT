be67fc8bf18d9800a3fb3a90247173b7454383ca
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-04-23 10:33:32 +0200

    name

--- ICT1512_JS/js02/project02/project02-02.js
+++ ICT1512_JS/js02/project02/project02-02.js
@@ -3,8 +3,8 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Zainap Van Blerck
+      Date:   2024/04/23
 
       Filename: project02-02.js
  */

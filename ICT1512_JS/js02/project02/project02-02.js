f542aeac44f610b04d86ff3155eb43ba7ea31b26
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-04-23 11:40:39 +0200

    completed pro2

--- ICT1512_JS/js02/project02/project02-02.js
+++ ICT1512_JS/js02/project02/project02-02.js
@@ -9,3 +9,19 @@
       Filename: project02-02.js
  */
  
+
+
+function verifyForm(){
+
+      let fName = document.getElementById("name").value;
+      let fEmail = document.getElementById("email").value;
+      let fPhone = document.getElementById("phone").value;
+
+      let message =(fName !== "" && fEmail !== "" && fPhone !== "") ? "Thank you" : "Please fill in all fields";
+
+      alert(message);
+
+      
+};
+
+document.getElementById("submitbutton").addEventListener("click", verifyForm);

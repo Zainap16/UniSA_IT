0340e435cab6532b28d02f68f7105c64a63e0d3a
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-05-03 13:49:37 +0200

    still busy

--- ICT1512_JS/js03/chapter/js03.js
+++ ICT1512_JS/js03/chapter/js03.js
@@ -21,3 +21,45 @@ let weekDays = [
   "Friday",
   "Saturday",
 ];
+
+window.addEventListener("load", addWeekDays);
+// Function to write weekday names into the calendar
+
+function addWeekDays() {
+  let i = 0; //intial counter value
+
+  // reference the collection of heading cells
+  let headingCells = document.getElementsByTagName("th");
+
+  // write each of the seven days into a heading cell
+  while (i < 7) {
+    headingCells[i].innerHTML = weekDays[i];
+    i++; // increase the counter by 1
+  }
+}
+
+window.addEventListener("load", showGames);
+
+// Function to write game information into the calendar
+
+function showGames() {
+  for (let i = 0; i < gameDates.length; i++) {
+    let gameInfo = "";
+    //open the paragraph
+    gameInfo += "<p>";
+
+    //include the opponent
+    gameInfo += gameOpponents[i] + "<br>";
+
+    //include the result and score
+    gameInfo +=
+      gameResults[i] + ": (" + runsScored[i] + " - " + runsAllowed[i] + ") ";
+
+    //close paragraph
+    gameInfo += "</p>";
+
+    //write the information into a table cell
+    let tableCell = document.getElementById(gameDates[i]);
+    tableCell.insertAdjacentHTML("beforeEnd", gameInfo);
+  }
+}
---
+++ somethingchp3.js
@@ -0,0 +1,9 @@
+console.log("<table>");
+for (let i = 1; i <= 2; i++) {
+    console.log("<tr>");
+ for (let j = 1; j <= 3; j++) {
+    console.log("<td>" + i + "," + j + "</td>");
+ }
+ console.log("</tr>");
+}
+console.log("</table>");

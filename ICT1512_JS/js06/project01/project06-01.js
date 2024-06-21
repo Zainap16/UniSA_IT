28acf82c4d9ab36fbc49daa38a4b816ebe46bbba
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-06-21 14:44:02 +0200

    eys

---
+++ ICT1512_JS/js06/project01/project06-01.js
@@ -0,0 +1,18 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-01
+
+      Project to validate a form used for setting up a new account
+      Author: Zainap Van Blerck
+      Date:   2024/06/21
+
+      Filename: project06-01.js
+*/
+
+let submitButton = document.querySelector("#submitButton");
+let pwd = document.querySelector("#pwd");
+let pwd2 = document.querySelector("#pwd2");
+
+
+

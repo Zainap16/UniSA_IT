ea253b1afbcb3b5b8e520a5047f50ab771bd2d49
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-06-25 15:11:00 +0200

    yes

--- ICT1512_JS/js06/project01/project06-01.js
+++ ICT1512_JS/js06/project01/project06-01.js
@@ -14,5 +14,16 @@ let submitButton = document.querySelector("#submitButton");
 let pwd = document.querySelector("#pwd");
 let pwd2 = document.querySelector("#pwd2");
 
+submitButton.addEventListener("click", function(){
+      if(pwd.validity.patternMismatch)
+      {
+            pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number");
+      }else if (pwd != pwd2){
 
+            pwd2.setCustomValidity("password does not match");
+
+      }else{
+            pwd.setCustomValidity("");
+      }
+})
 

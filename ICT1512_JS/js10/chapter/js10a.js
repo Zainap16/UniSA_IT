a8ba6faa968a91ed95d5dca787da973a1c687be8
Ziah <122858207+Zainap16@users.noreply.github.com>
2024-08-16 08:30:32 +0200

    added ch10

---
+++ ICT1512_JS/js10/chapter/js10a.js
@@ -0,0 +1,45 @@
+"use strict";
+/*  JavaScript 7th Edition
+    Chapter 10
+    Chapter case
+
+    Oak Top House
+    Author: 
+    Date:   
+
+    Filename: js10a.js
+*/
+
+
+window.addEventListener("load", setupRoom);
+
+// perform setup tasks when page first loads
+function setupRoom() {
+   //Page objects
+   let room = document.getElementById("room");                            // banquet hall
+   let storage = document.getElementsByTagName("storage");                // storage room
+   let roomTables = document.querySelectorAll("#room > div.table");       // Tables in the banquet hall
+   let storageTables = document.querySelectorAll("#storage > div.table"); // Tables in the storage room
+   let zIndexCounter = 0;                                                 // Count the highest z-Index value
+   
+   // Function to calculate available seats in the room layout
+   function countSeats() {
+      let guests = 0;
+            let seatCount = document.getElementById("seatCount");      
+      let tablesToCount = document.querySelectorAll("#room > div.table");
+      for (let  items of tablesToCount) {
+         guests += parseInt(items.textContent);
+      }
+      seatCount.textContent = guests;
+    }
+
+    // Add tables from storage to the banquet hall
+for (let items of storageTables) {
+   items.onclick = function() {
+   let storageCopy = items.cloneNode(true);
+   room.appendChild(storageCopy); 
+   zIndexCounter++;
+storageCopy.style.zIndex = zIndexCounter; 
+countSeats();
+}
+}} 
